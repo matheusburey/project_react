01216890f01db82a9b4f75b9b8ffd3c07842ddb1
matheusburey <matheushburei@gmail.com>
2022-05-15 08:21:04 -0300

    feat: created atom select component

---
+++ src/components/atoms/Select/index.tsx
@@ -0,0 +1,12 @@
+function Select() {
+  return (
+    <select className="border-black border-2 rounded-lg h-11 w-32 text-center bg-neutral-100 cursor-pointer hover:border-orange-600 focus:border-orange-600-500 focus:outline-none">
+      <option value="011">011</option>
+      <option value="016">016</option>
+      <option value="017">017</option>
+      <option value="018">018</option>
+    </select>
+  );
+}
+
+export default Select;
