60743a2f8e6e05b6087936ea5438ef65d4d3ef47
matheusburey <matheushburei@gmail.com>
2022-05-15 08:26:48 -0300

    refactor: import form component and created page title

--- src/pages/Planos/index.tsx
+++ src/pages/Planos/index.tsx
@@ -1,5 +1,16 @@
+import ColorText from "../../components/atoms/ColorText";
+import Form from "../../components/organisms/Form";
+
 function Planos() {
-  return <div>Planos</div>;
+  return (
+    <div>
+      <h1 className="py-10 text-center text-4xl">
+        Qual plano <ColorText>Falemais</ColorText> se encaixa com{" "}
+        <ColorText>Você</ColorText>?
+      </h1>
+      <Form />
+    </div>
+  );
 }
 
 export default Planos;
