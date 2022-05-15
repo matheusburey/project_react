e4c8d50bb3cc6d51c5fc119b2fd906407acf5f4c
matheusburey <matheushburei@gmail.com>
2022-05-15 08:14:56 -0300

    refactor: altaration props button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,10 +1,11 @@
 interface IButtonProps {
   children: string;
   isBlack?: boolean;
-  callback: () => void;
+  callback?: () => void | undefined;
+  type?: "button" | "submit";
 }
 
-function Button({ children, callback, isBlack }: IButtonProps) {
+function Button({ children, callback, isBlack, type }: IButtonProps) {
   const backgroundButton = isBlack
     ? "bg-black text-neutral-100"
     : "bg-neutral-100";
@@ -12,7 +13,8 @@ function Button({ children, callback, isBlack }: IButtonProps) {
   return (
     <button
       className={`${backgroundButton} w-32 h-11 m-4 border-black border-2 rounded-lg  hover:bg-orange-600 hover:text-neutral-100 hover:border-orange-600 transition-colors`}
-      type="button"
+      // eslint-disable-next-line react/button-has-type
+      type={type}
       onClick={callback}
     >
       {children}
@@ -22,6 +24,8 @@ function Button({ children, callback, isBlack }: IButtonProps) {
 
 Button.defaultProps = {
   isBlack: false,
+  type: "button",
+  callback: undefined,
 };
 
 export default Button;
