9c2274c9f23e4d9d3d52544dce22a2d9c9cd40bc
matheusburey <matheushburei.99@gmail.com>
2022-05-16 10:35:52 -0300

    feat: created schema to valid form

--- src/components/organisms/Form/index.tsx
+++ src/components/organisms/Form/index.tsx
@@ -1,36 +1,53 @@
-import { FormEvent } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
+import { PlanCalcUse } from "../../../providers/PlanCalc";
 import Button from "../../atoms/Button";
-import Select from "../../atoms/Select";
-import Label from "../../molecules/Label";
-import PlanOptions from "../../molecules/PlanOptions";
+import Input from "../../molecules/Input";
+import Select from "../../molecules/Select";
 
 function Form() {
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(event);
-  };
+  const { calculate } = PlanCalcUse();
+  const optionsDDD = ["011", "016", "017", "018"];
+  const optionsPlan = ["FaleMais30", "FaleMais60", "FaleMais120"];
+
+  const schema = yup.object().shape({
+    plan: yup.string().required("Valor obrigatorio!"),
+    origin: yup.string().required("Valor obrigatorio!"),
+    destiny: yup.string().required("Valor obrigatorio!"),
+    minute: yup.string().required("Valor obrigatorio!"),
+  });
+
+  const { register, handleSubmit } = useForm({ resolver: yupResolver(schema) });
+
   return (
     <form
-      onSubmit={handleSubmit}
-      className="max-w-[700px] m-auto border-black border-2 rounded-lg"
+      onSubmit={handleSubmit(calculate)}
+      className="max-w-[700px] flex justify-around flex-wrap m-auto border-black border-2 rounded-lg"
     >
-      <PlanOptions />
-      <div className="flex justify-around items-center mt-4">
-        <Label text="origen">
-          <Select />
-        </Label>
-        <Label text="Destino">
-          <Select />
-        </Label>
-        <Label text="minutos">
-          <input
-            type="number"
-            className="w-32 h-11 bg-neutral-100 border-black border-2 rounded-lg text-center cursor-pointer hover:border-orange-600 focus:border-orange-600-500 focus:outline-none"
-          />
-        </Label>
-        <Button type="submit">Simular uso</Button>
-      </div>
+      <Select
+        label="Planos"
+        register={register}
+        name="plan"
+        options={optionsPlan}
+      />
+      <Select
+        label="Origen"
+        register={register}
+        name="origin"
+        options={optionsDDD}
+      />
+      <Select
+        label="Destino"
+        register={register}
+        name="destiny"
+        options={optionsDDD}
+      />
+      <Input label="Minutos" register={register} type="number" name="minute" />
+      <Button type="submit" isBlack>
+        Simular uso
+      </Button>
     </form>
   );
 }
