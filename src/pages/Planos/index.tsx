d5d6e05da225e09949150559c2b00ccad46bc016
matheusburey <matheushburei.99@gmail.com>
2022-05-16 10:44:05 -0300

    feat: add tag result

--- src/pages/Planos/index.tsx
+++ src/pages/Planos/index.tsx
@@ -1,14 +1,16 @@
 import ColorText from "../../components/atoms/ColorText";
 import Form from "../../components/organisms/Form";
+import Result from "../../components/organisms/Result";
 
 function Planos() {
   return (
-    <div>
+    <div className="p-2">
       <h1 className="py-10 text-center text-4xl">
         Qual plano <ColorText>Falemais</ColorText> se encaixa com{" "}
         <ColorText>Você</ColorText>?
       </h1>
       <Form />
+      <Result />
     </div>
   );
 }
