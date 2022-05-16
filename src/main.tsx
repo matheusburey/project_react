567b2928da272254f2265d3d5a906d5a4a1b6aeb
matheusburey <matheushburei.99@gmail.com>
2022-05-16 10:40:24 -0300

    feat: add tag provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import Provider from "./providers";
 import "./style/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Provider>
+				<App />
+			</Provider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
